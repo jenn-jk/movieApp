ca369bfe45e0f06e44276da3e8def27cd3994c18
jenn-jk <jenkins051@gmail.com>
2025-07-20 12:13:48 +0530

    added movie posters

--- lib/pages/movie_details.dart
+++ lib/pages/movie_details.dart
@@ -8,12 +8,33 @@ class MovieDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Movie Details")), body: Container(
-      child: Column(
-        children: [
-          MovieCard(movie: movie)
-        ],
+    return Scaffold(
+      appBar: AppBar(title: Text("Movie Details")),
+      body: Container(
+        child: Column(
+          children: [
+            MovieCard(movie: movie),
+            Text(
+              "Movie Posters",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: movie.images.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Image(
+                    image: NetworkImage(movie.images[index]),
+                    fit: BoxFit.contain,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
